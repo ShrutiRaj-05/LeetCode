1e339255b2c71186320962d19c8f66a31372ba03
Shruti Chaudhari <shrutirajchaudhari@gmail.com>
2026-09-22 10:58:15 +0530

    Time: 281 ms (38.21%), Space: 120.8 MB (46.44%) - LeetHub

---
+++ 2630-memoize-ii/2630-memoize-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    return function(...args) {
+        let current = cache;
+        for(const arg of args){
+            if(!current.has(arg)){
+                current.set(arg, new Map());
+            }
+            current = current.get(arg);
+        }
+        if(current.has("result")){
+            return current.get("result");
+        }
+        const result = fn(...args);
+        current.set("result", result);
+        return result;
+    };
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
